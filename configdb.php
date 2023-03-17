e160af07467d13f0bfded11f10843cbc67708b99
CHAMLEE <chamlee@clika.net>
2023-03-16 18:53:25 -0700

    Se agrega php para configurar BD

---
+++ configdb.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'lamp-db');
+   define('DB_USERNAME', 'scannerbreeze');
+   define('DB_PASSWORD', 'k7R6sQU2XFfMpPU8xKn1');
+   define('DB_DATABASE', 'carros');
+   $con = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
