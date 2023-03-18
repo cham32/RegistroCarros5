0768c1223f513cb2298af398de9609109566040f
CHAMLEE <chamlee@clika.net>
2023-03-17 18:35:38 -0700

    Proyecto prefinalizado

--- agregarusuario.php
+++ agregarusuario.php
@@ -1,15 +1,15 @@
 <?php
 $usuario = $_POST['usuario'];
 $correo =$_POST['correo'];
-$contraseña = md5($_POST['contraseña']);
+$contraseña = md5($_POST['contra']);
 $rol =$_POST['rol'];
 
 include("./configdb.php");
 if (!$con)
 die('No se pudo conectar: ' . mysqli_error($con));
 
-$sql = "INSERT INTO usuarios(usuario, correo, contraseña)";
-$sql = $sql." VALUES('".$usuario."','".$correo."','".$contraseña."')";
+$sql = "INSERT INTO usuarios(usuario, correo, contraseña, rol)";
+$sql = $sql." VALUES('".$usuario."','".$correo."','".$contraseña."',".$rol.")";
 $result = mysqli_query($con,$sql);                     
 mysqli_close($con);
 ?>
